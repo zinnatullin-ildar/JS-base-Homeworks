ff3efcdf82570dd577f06a8999c1f0d1ce53d686
Ildar Zinnatullin <iqz@izh.com>
2022-05-16 00:19:35 +0400

    ДЗ к лекции 1 «Основные понятия»

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,51 @@
+"use strict"
+
+// Задача №1
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D === 0) {
+    arr.push(-b / (2 * a));
+  } else if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
+  }
+  return arr;
+}
+
+
+// Задача №2
+
+function monthTotal(dateFrom, dateTo) {
+  let diff = dateTo.getMonth() - dateFrom.getMonth() + 12 * (dateTo.getFullYear() - dateFrom.getFullYear());
+  return (diff <= 0) ? 1 : diff;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount = 0;
+  if (isNaN(parseFloat(percent))) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+  else if (isNaN(parseFloat(contribution))) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  else if (isNaN(parseFloat(amount))) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  else if (!(date instanceof Date)) {
+    return `"Срок ипотеки" содержит неправильное значение "${date}"`;
+  } else {
+    let debtAmount = amount - contribution;
+    let P = (percent / 100) / 12;
+    let now = new Date();
+    let creditTerm = monthTotal(now, date);
+    let monthlyPayment = debtAmount * (P + (P / (Math.pow((1 + P), creditTerm) - 1)));
 
-  // код для задачи №2 писать здесь
+    totalAmount = +(monthlyPayment * creditTerm).toFixed(2);
+    console.log(`Сумма по кредиту составила: ${totalAmount} рублей`);
 
-  return totalAmount;
+    return totalAmount;
+  }
 }
