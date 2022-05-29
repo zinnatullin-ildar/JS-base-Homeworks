a969da9ca68ad0d6a603c7e8a71e5316ec39c140
Ildar Zinnatullin <iqz@izh.com>
2022-05-30 00:46:24 +0400

    ДЗ к лекции 2 «Функции»

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+avg = (sum / arr.length).toFixed(2);
+return { min: min, max: max, avg: +avg }
 }
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let value = 0;
+  let sum = arr.reduce(function (a, b) {
+    return a + b;
+  }, value);
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+let max = func(arrOfArr[0]);
+  for (i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i])
+    }
+  }
   return max;
 }
 
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return Math.abs(Math.max(...arr) - Math.min(...arr))
 }
