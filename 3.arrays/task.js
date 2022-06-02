1a1276e6e2b410ed5c2a67a99862f28fafd07ff3
Ildar Zinnatullin <iqz@izh.com>
2022-06-02 09:00:03 +0400

    ДЗ к лекции 3 «Массивы»

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
+// Задание 1
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+  return result;  
 }
 
+// Задание 2
+
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
