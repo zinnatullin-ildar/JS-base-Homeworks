c181626ba70f98059fa4c807ad3e5be05e8142bb
Ildar Zinnatullin <iqz@izh.com>
2022-06-13 23:18:09 +0400

    ДЗ к лекции 5 «Классы»

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -27,12 +27,12 @@ class PrintEditionItem {
     }
 }
 
-// const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
 
-// console.log(sherlock.releaseDate); //2019
-// console.log(sherlock.state); //100
-// sherlock.fix();
-// console.log(sherlock.state); //100
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
 
 class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
@@ -70,13 +70,13 @@ class DetectiveBook extends Book {
     }
 }
 
-// const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
 
-// console.log(picknick.author); //"Аркадий и Борис Стругацкие"
-// picknick.state = 10;
-// console.log(picknick.state); //10
-// picknick.fix();
-// console.log(picknick.state); //15
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state); //15
 
 // Задание 2
 
@@ -102,19 +102,19 @@ class Library {
     }
 }
 
-// const library = new Library("Библиотека имени Ленина");
+const library = new Library("Библиотека имени Ленина");
 
-// library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
-// library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
-// library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-// library.addBook(new Magazine("Мурзилка", 1924, 60));
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
 
-// console.log(library.findBookBy("name", "Властелин колец")); //null
-// console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
 
-// console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-// library.giveBookByName("Машина времени");
-// console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
 
 // Задание 3
 
@@ -162,13 +162,13 @@ class Student {
     };
 }
 
-// const student = new Student("Олег Никифоров");
-// student.addMark(5, "algebra");
-// student.addMark(5, "algebra");
-// student.addMark(5, "geometry");
-// student.addMark(4, "geometry");
-// student.addMark(6, "geometry"); // "Ошибка, оценка должна быть числом от 1 до 5"
-// student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
-// student.getAverageBySubject("biology"); // Несуществующий предмет
-// student.getAverage(); // Средний балл по всем предметам 4.75
-// student.exclude("Исключен за попытку подделать оценки");
+const student = new Student("Олег Никифоров");
+student.addMark(5, "algebra");
+student.addMark(5, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+student.addMark(6, "geometry"); // "Ошибка, оценка должна быть числом от 1 до 5"
+student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student.getAverageBySubject("biology"); // Несуществующий предмет
+student.getAverage(); // Средний балл по всем предметам 4.75
+student.exclude("Исключен за попытку подделать оценки");
