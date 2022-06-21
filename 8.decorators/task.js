662752e4cc66fc290184dfe23ae9e420e4033ca9
Ildar Zinnatullin <iqz@izh.com>
2022-06-21 23:12:43 +0400

    ДЗ к лекции 8 "Функции декораторы"

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,67 @@
+// Задание 1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(","); // получаем правильный хэш
+    let result = cache.find((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (result) { // если элемент найден
+      console.log("Из кэша: " + result.value); // индекс известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + result.value;
+    }
+
+    let value = func(...args); // в кэше результата нет, нужно считать
+    cache.push({ hash, value }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift() // если в кэше слишком много элементов, то удаляем самый старый (первый)
+    }
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;
+  }
+  return wrapper;
 }
 
+// Задание 2
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+  let timerId = null;
+
+  function wrapper(...args) {
+    if (!flag) {
+      func(args);
+      flag = true;
+    }
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func(args);
+      flag = false;
+    }, ms)
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+// Задание 3
+
+function debounceDecorator2(func, ms) {
+  let flag = false;
+  let timerId = null;
+  wrapper.count = 0;
+
+  function wrapper(...args) {
+    wrapper.count++
+    if (!flag) {
+      func(args);
+      flag = true;
+    }
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func(args);
+      flag = false;
+    }, ms)
+  }
+  return wrapper;
 }
